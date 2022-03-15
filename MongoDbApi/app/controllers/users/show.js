e523bda2599a6278e0b58e6d8339227f9db1a0d1
C-Lilian <76873182+C-Lilian@users.noreply.github.com>
2022-03-15 18:26:23 +0100

    Add files via upload

---
+++ MongoDbApi/app/controllers/users/show.js
@@ -0,0 +1,49 @@
+const UserModel = require('../../models/user.js');
+
+const ShowMany = class ShowMany {
+  /**
+   * @constructor
+   * @param {Object} app 
+   * @param {Object} config 
+   */
+  constructor (app, connect, config) {
+    this.app = app;
+    this.UserModel = connect.model('User', UserModel);
+    this.run()
+  }
+
+  /**
+   * Middleware
+   */
+  middleware() {
+    this.app.get('/users/show', (req, res) => {
+      try {
+        // Regarder la méthode findOne 
+        this.UserModel.find(req.params).then(user => {
+          res.status(200).json(user || {})
+        }).catch(() => {
+          res.status(500).json({
+            code: 500,
+            message: 'Internal server error'
+          })
+        })
+      } catch (error) {
+        console.error(`[ERROR] users/show/:id -> ${error}`)
+
+        res.status(400).json({
+          'code': 400,
+          'message': 'Bad request'
+        })
+      }
+    })
+  }
+
+  /**
+   * Run
+   */
+  run() {
+    this.middleware()
+  }
+}
+
+module.exports = ShowMany;
